7f760a09292c341b000cd56782d380ae863b6a28
ahadsts9901 <ahadsts990@gmail.com>
2023-03-12 06:24:48 -0700

    loops remaining

---
+++ main.js
@@ -0,0 +1,37 @@
+// let cities = ['karachi', 'islamabad', 'quetta', 'peshawar' ]
+
+// alert ('welcome to: ' + cities[1])
+
+
+
+
+
+// let countries = []
+
+//     countries[1] = 'india'
+//     countries[0] = 'pakistan'
+//     countries[2] = 'australia'
+//     countries[3] = 'england'
+
+//     countries.pop()
+//     countries.push('srilanka', 'bangladesh')
+
+//     alert (countries[4])
+
+
+
+
+// let sports = ['cricket', 'football', 'boxing', 'cycling']
+
+//     sports.shift()
+//     sports.unshift('hockey', 'badminton')
+
+//     sports.splice(0, 1, 'wrestling', 'gaming')
+//     let nosports = sports.slice(0, 1)
+
+//     alert (sports[1])
+//     alert (nosports)
+
+
+
+
